5f707caa0db50322946ecb289d02951114bc02bd
Yousef Kadry <yousefkadry01@gmail.com>
2023-10-24 13:15:40 +0300

    [ FEAT ]: creat get average func

--- math.cpp
+++ math.cpp
@@ -10,9 +10,16 @@ int getSum(vector<int> vec){
     return sum;
 }
 
+int getAverage(vector<int> vec){
+    int sum = getSum(vec);
+    int avg = sum / vec.size();
+    return avg;
+}
 int main(){
     vector<int> myVector(10, 5);
     int vectorSum = getSum(myVector);
-    cout<< "rsults = " << vectorSum << endl;
+    int vectorAvg = getAverage(myVector);
+    cout<< "Sum = " << vectorSum << endl;
+    cout<< "Average = " << vectorAvg << endl;
     return 0;
 }
